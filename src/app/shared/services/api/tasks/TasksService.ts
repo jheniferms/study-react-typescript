5959f40c46bfb1755dae62c38927a2a536538c15
Jhenifer <jhenifer.msantos@gmail.com>
2024-02-08 14:08:14 -0300

    feat: create service

---
+++ src/app/shared/services/api/tasks/TasksService.ts
@@ -0,0 +1,68 @@
+import { API } from "../ApiConfig";
+import { ApiException } from "../ApiException";
+
+interface ITask {
+    title: string;
+    isCompleted: boolean;
+    id: number
+
+}
+const getAll = async (): Promise<ITask[] | ApiException> => {
+    try {
+        const { data } = await API().get("/tasks");
+        return data
+
+    } catch (error: any) {
+        return new ApiException(error.message || "Error getting tasks");
+    }
+};
+
+const get = async (id: number): Promise<ITask | ApiException> => {
+    try {
+        const { data } = await API().get(`/tasks/${id}`);
+        return data
+
+    } catch (error: any) {
+        return new ApiException(error.message || "Error getting task");
+    }
+};
+
+const deleteById = async (id: number): Promise<undefined | ApiException> => {
+    try {
+       await API().delete(`/tasks/${id}`);
+        return undefined;
+
+    } catch (error: any) {
+        return new ApiException(error.message || "Error deleting task");
+    }
+};
+
+const create = async (request: Omit<ITask, "id">): Promise<ITask | ApiException> => {
+    try {
+        const { data } = await API().post(`/tasks`, request);
+        return data
+
+    } catch (error: any) {
+        return new ApiException(error.message || "Error creating task");
+    }
+};
+
+const update = async (id: number, request: ITask): Promise<ITask | ApiException> => {
+    try {
+        const { data } = await API().put(`/tasks/${id}`, request);
+        return data
+
+    } catch (error: any) {
+        return new ApiException(error.message || "Error updating task");
+    }
+};
+
+
+
+export const TasksService = {
+    getAll,
+    get,
+    create,
+    update,
+    deleteById
+}
